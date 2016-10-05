2b7dbb8a769cd2168a95ddc6c4360b04ce984340
Umur_Semih_KALEM <kalemsemih@gmail.com>
2016-10-05 21:05:51 +0300

    odev2

--- odev/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="odev.MvcApplication" Language="C#" %>
---
+++ odev2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(odev2.Startup))]
+namespace odev2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
